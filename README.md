bbf852ca62ac539e665cad2696ca1ae12b7cbfbf
Marek Aufart <aufi.cz@gmail.com>
2016-10-01 12:46:01 +0200

    add csv export

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Categorizer, Prague hacks 2016 project
 
+## Steps
+
+```$ git clone git@github.com:aufi/categorizer.git```
+
+```$ cd categorizer/data_process && bundle```
+
+Create data directory with data from Lukas and return to categorizer directory
+
+```$ ruby data_process/01_load_to_es.rb```
+
+```$ ruby data_process/02_features_regex.rb```
+
+```$ ruby data_process/03_export_csv.rb```
 
 ## Notes
+
+Requirements: Ruby
 
 elasticsearch local endpoint http://10.2.22.233:9200/_plugin/head/
--- data_process/03_export_csv.rb
+++ data_process/03_export_csv.rb
@@ -6,16 +6,27 @@ ES = Elasticsearch::Client.new url: 'http://10.2.22.233:9200'
 d = ES.search index: 'featured_documents', body: { query: {"query":{"match_all":{}}}, size: 9999, from: 0 } # size: 9999
 
 def select_features(attrs)
-  # TODO
-  []
+  features = []
+  attrs.each_pair do |k, v|
+    features << v if k.start_with? 'feature_'
+  end
+  features
+end
+
+def select_feature_names(attrs)
+  features = ['id']
+  attrs.each_pair do |k, v|
+    features << k if k.start_with? 'feature_'
+  end
+  features
 end
 
 data = d['hits']['hits']
 
-csv_string = CSV.generate do |csv|
+csv_string = CSV.generate(write_headers: true, headers: select_feature_names(data.first["_source"])) do |csv|
   data.each do |d|
     doc = d["_source"]
-    csv << [d["_id"], select_features(doc)]
+    csv << [d["_id"], *select_features(doc)]
   end
 end
 
