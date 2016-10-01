7a3e3056e75f470dbb12411762eefb6dc471a157
Marek Aufart <aufi.cz@gmail.com>
2016-10-01 17:47:33 +0200

    add dummy pagination to handle 240k docs

--- data_process/02_features_regex.rb
+++ data_process/02_features_regex.rb
@@ -9,8 +9,6 @@ REGEXP_SELECTORS = [
   {name: 'eia', regex: /životní prostředí/},
 ]
 
-d = ES.search index: 'raw_documents', body: { query: {"query":{"match_all":{}}}, size: 9999, from: 0 } # size: 9999
-
 def assign_regex_cat(record)
   doc = record["_source"]
   data = {}
@@ -23,10 +21,16 @@ def assign_regex_cat(record)
   ES.index index: 'featured_documents', type: 'document', id: record['_id'], body: data
 end
 
-data = d['hits']['hits']
+step = 9999
 
-data.each do |d|
-  assign_regex_cat(d)
+0..25.times do |i| # fixed document count, do it better in future
+  d = ES.search index: 'raw_documents', body: { query: {"query":{"match_all":{}}}, size: step, from: i * step }
+
+  data = d['hits']['hits']
+
+  data.each do |d|
+    assign_regex_cat(d)
+  end
+
+  p data.count
 end
-
-p data.count
