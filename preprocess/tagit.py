95c317ea370b8fdc14d77e1a1e5e21d192da292a
Marek Aufart <aufi.cz@gmail.com>
2016-10-02 12:26:05 +0200

    add keywords vec

--- preprocess/tagit.py
+++ preprocess/tagit.py
@@ -1,8 +1,14 @@
 import glob
 import json
+import math
 import re
 
 titles = json.load(open("../titles.id.txt"))
+
+keywords = []
+for line in open('keywords.regexp').read().split("\n"):
+    if line != '':
+        keywords.append(r'%s' % line)
 
 class Classifier:
     patterns = []
@@ -25,6 +31,20 @@ class Classifier:
                 categories.append(pattern['category'])
         return categories
 
+    def features_vec(self, text):
+        keywords_vec = []
+        regexp_vec   = []
+        sizes_vec    = [
+            min([1, math.log(len(text),10)/10]),
+            min([1, math.log(len(re.split(text, r'\s+')),10)/10]),
+            min([1, math.log(len(re.split(text, r'\n\s\n')),10)/10])
+        ]
+        for k in keywords:
+            keywords_vec.append(1 if re.search(k, text, re.IGNORECASE) else 0)
+        for k in self.patterns:
+            regexp_vec.append(1 if re.search(k, text, re.IGNORECASE) else 0)
+        return keywords_vec + regexp_vec + sizes_vec
+
 
 c = Classifier()
 
@@ -43,4 +63,6 @@ for fname in glob.glob("../data/*/*.txt"):
     id = re.search(r'\d+\/(\d+)\.txt', fname).group(1)
     title = titles[id] # fails when not found
     body = open(fname).read()
-    get_categories(id, title, "\n".join([title, body]))
+    content = '\n'.join([title, body])
+    #get_categories(id, title, content)
+    print(c.features_vec(content)) #.unshift(id).join(',')
